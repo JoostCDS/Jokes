2b21b557189da7444dcb4e5b8827d3d89037bf84
JoostCDS <joost@codaisseur.com>
2020-05-15 16:19:39 +0200

    first version

---
+++ index.js
@@ -0,0 +1,85 @@
+// import express
+const express = require("express");
+
+// create express server
+const app = express();
+
+//creating hmtl page
+
+function createSomeJokes(age, gender) {
+  let myObject = {
+    joke1: `What is green and goes down the slopes very fast`,
+    joke2: `What is green and goes down the slopes very fast`,
+    joke3: `What is green and goes down the slopes very fast`,
+    joke4: `What is green and goes down the slopes very fast`,
+    joke5: `What is green and goes down the slopes very fast`,
+    joke6: `What is green and goes down the slopes very fast`,
+  };
+  const document = `<html>
+  <head>
+    <title>home</title>
+  </head>
+  
+  <body>
+    <h1>Welcome Joke lover !!</h1>
+    <br>
+    ${myObject.joke1}
+  </body>
+  </html> `;
+  return document;
+}
+
+app.get("/:age/:gender", (request, response) => {
+  const age = request.params.age;
+  const gender = request.params.gender;
+
+  const document = createSomeJokes(age, gender);
+  response.send(document);
+});
+
+// handler function
+function render(message) {
+  const document = `
+    <html>
+    <head>
+      <title>home</title>
+    </head>
+    
+    <body>
+      <h1>Welcome ${message} ?!!</h1>
+      <br>
+      we're so happy to see you here!
+    </body>
+    </html>`;
+  return document;
+}
+
+// app.get("/user/:name", (request, response) => {
+//   const message = request.params.name;
+//   const page = render(message);
+
+//   response.send(page);
+// });
+
+// register GET /hello endpoint
+// app.get(
+//   "/", // route to listen on
+//   onRequest // callback runs when the route is requested
+// );
+// app.get(
+//     '/user/:name',
+//     response.send(param)
+// app.get(
+//   "/test", // route
+//   (request, response) => {
+//     // handler callback
+//     console.log(request.path);
+//     response.send(page);
+//   }
+// );
+
+// 3000 is common
+const port = 4000;
+
+// start listening
+app.listen(port, () => console.log(`Listening on :${port}`));
